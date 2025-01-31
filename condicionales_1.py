3be149fcfaec003d9aaa8f2bfae2acca9fb6de6b
Eyersson <eyerssonmontana@colegioguanenta.edu.co>
2025-01-31 09:11:39 -0500

    se modifico el diagrama

---
+++ condicionales_1.py
@@ -0,0 +1,17 @@
+#input
+cant_minuto=int(input("digite la cant_minuto ") )
+
+# processing
+if cant_minuto <=3:
+    valor_llamada = 300 
+
+else:valor_llamada=300*+50*(cant_minuto)
+
+#output
+
+print("El valor de la llamada es:"+str(valor_llamada))
+
+
+
+
+
